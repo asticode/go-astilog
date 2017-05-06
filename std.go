3047d9a1a9276c3129b1f8e43fad07a09456bed3
Quentin Renard <contact@asticode.com>
2017-05-06 13:30:47 +0200

    global logger is nop logger

--- std.go
+++ std.go
@@ -1,7 +1,7 @@
 package astilog
 
 // Global logger
-var gb = New(Configuration{AppName: "unknown"})
+var gb = NopLogger()
 
 // SetLogger sets the global logger
 func SetLogger(l Logger) {
@@ -9,13 +9,13 @@ func SetLogger(l Logger) {
 }
 
 // Global logger shortcuts
-func Debug(v ...interface{}) { gb.Debug(v...) }
+func Debug(v ...interface{})                 { gb.Debug(v...) }
 func Debugf(format string, v ...interface{}) { gb.Debugf(format, v...) }
-func Info(v ...interface{}) { gb.Info(v...) }
-func Infof(format string, v ...interface{}) { gb.Infof(format, v...) }
-func Warn(v ...interface{}) { gb.Warn(v...) }
-func Warnf(format string, v ...interface{}) { gb.Warnf(format, v...) }
-func Error(v ...interface{}) { gb.Error(v...) }
+func Info(v ...interface{})                  { gb.Info(v...) }
+func Infof(format string, v ...interface{})  { gb.Infof(format, v...) }
+func Warn(v ...interface{})                  { gb.Warn(v...) }
+func Warnf(format string, v ...interface{})  { gb.Warnf(format, v...) }
+func Error(v ...interface{})                 { gb.Error(v...) }
 func Errorf(format string, v ...interface{}) { gb.Errorf(format, v...) }
-func Fatal(v ...interface{}) { gb.Fatal(v...) }
+func Fatal(v ...interface{})                 { gb.Fatal(v...) }
 func Fatalf(format string, v ...interface{}) { gb.Fatalf(format, v...) }
