573b89b30c0abe5b552cf704951ed0b58a442d53
Quentin Renard <contact@asticode.com>
2020-01-14 10:25:45 +0100

    Added travis support

---
+++ .travis.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+if [[ $(go list -m all) =~ ^github.com/asticode/go-astilog[[:space:]]+github.com/asticode/go-astikit[[:space:]]+v[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+$ ]]; then
+    echo "cheers"
+else
+    echo "This repo doesn't allow other dependencies than astikit"
+    exit 1
+fi
